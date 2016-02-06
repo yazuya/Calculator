5040d7382dd3a152afbc484b918c465f6794083b
yazuya <une_personne_libre3@softbank.ne.jp>
2016-02-06 17:00:28 +0900

    calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label : UILabel!
+    
+    var number: Int = 0
+    var number2: Int = 0
+    var operation: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,38 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func select3(){
+        number = number*10 + 3
+        label.text = String(number)
+    }
+    
+    @IBAction func select4(){
+        number = number*10 + 4
+        label.text = String(number)
+    }
+    
+    @IBAction func plus(){
+        label.text = "0"
+        
+        operation = 1
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func equal(){
+        if operation == 1{
+            label.text = String(number + number2)
+            
+        }
+    }
+    
+    @IBAction func Clear(){
+        number = 0
+        number2 = 0
+        
+        label.text = "0"
     }
 
 
